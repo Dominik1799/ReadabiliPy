d017aa98ac44d5793e5fd77fc101e5dd46aba759
Dominik Horvath <dom.horvath17@gmail.com>
2024-03-09 16:48:57 +0100

    add the possibility to use isProbablyReadable method

---
+++ readabilipy/javascript/CheckReadability.js
@@ -0,0 +1,33 @@
+/*
+ * This file is part of ReadabiliPy
+ */
+
+const fs = require('fs');
+const { isProbablyReaderable } = require('@mozilla/readability');
+const { JSDOM } = require('jsdom');
+
+function readFile(filePath) {
+	return fs.readFileSync(filePath, {encoding: "utf-8"}).trim();
+}
+
+
+function main() {
+
+	var argv = require('minimist')(process.argv.slice(2));
+	if (argv['i'] === undefined) {
+		console.log("Input file required.");
+		return 1;
+	}
+
+	var inFilePath = argv['i'];
+
+	var html = readFile(inFilePath);
+	var doc = new JSDOM(html);
+	if (isProbablyReaderable(doc.window.document, { minScore: 20 })) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+main();
